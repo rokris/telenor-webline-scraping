6584468607a3cdb1bfbd151d3fbdd51fc514025f
Roger Kristiansen <roger.kristiansen@norgesgruppen.no>
2021-11-08 18:16:15 +0100

    code cleanup

--- get-webline-data.py
+++ get-webline-data.py
@@ -2,14 +2,24 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+# Access cookie from Webline
+authsession = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzY0MzQwMDAsImV4cGlyYXRpb24iOjEwODAwLCJsYXN0X3JlbW90ZV9pcCI6IjgxLjE2Ni4yMDIuMjI3Iiwib3RwIjoiZmxhc2gtc21zIiwicmVtb3RlX2lwIjoiODEuMTY2LjIwMi4yMjciLCJzaWQiOiIyRkRDNTk4QS00MDkxLTExRUMtOEVFMy1DMzUxQjFERTQwQjAiLCJ1c2VybmFtZSI6InJvZ2Vrcmk1In0.RfgX23FRw4rNkhmQNSkAEi-WJkqkTExO92gRmjK2wqM"
+base_url = "https://webline.telenor.no/mod-perl/webline"
+cookies = {'authsession': authsession}
+payload = {'wlkid':'3633215',
+           'id_customer':'1825',
+           'Search':'Please%20wait...',
+           'id_vpn':'1115925',
+           'type':'partner',
+           'cmd':'html_customer_vpn'}
 
-import_path = './webline.html'
 export_path = './export_telenor_webline.csv'
 
-with open(import_path, 'r', encoding = "ISO-8859-1") as f:
-    contents = f.read()
-    soup = BeautifulSoup(contents, 'html5lib') # Parse the HTML as a string
+# Get the date from Webline webserver
+r = requests.post(base_url, data=payload, cookies=cookies)
 
+# Create a nice Soup of the result
+soup = BeautifulSoup(r.content, 'html5lib') # Parse the HTML as a string
 table = soup.find_all('table')[4] # Grab a table
 rows = table.find_all('td') # Find the data
 
