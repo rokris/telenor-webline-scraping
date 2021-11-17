914e541034e13ae4ccca1f778bcd9ceabdd93f88
Roger Kristiansen <roger.kristiansen@norgesgruppen.no>
2021-11-17 20:41:18 +0100

    Cleanup

--- get-webline-data-all.py
+++ get-webline-data-all.py
@@ -1,42 +1,40 @@
-import requests
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
 # Access cookie from Webline
 authsession = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzcyMTE2MDAsImV4cGlyYXRpb24iOjEwODAwLCJsYXN0X3JlbW90ZV9pcCI6Ijc3LjE4LjUwLjcxIiwib3RwIjoiZmxhc2gtc21zIiwicmVtb3RlX2lwIjoiNzcuMTguNTAuNzEiLCJzaWQiOiI2Nzk2RUM4MC00N0FBLTExRUMtQTE1OS1GODY2QjFERTQwQjAiLCJ1c2VybmFtZSI6InJvZ2Vrcmk1In0.yd3l2W5IuaLuEij_U7VQZf-wm1iv7AwNaPiQKSqrF6o"
 base_url = "https://webline.telenor.no/mod-perl/webline"
-cookies = {'authsession': authsession}
+cookies = {"authsession": authsession}
 
-## All VPN
+# All VPN
 payload = {
-    'wlkid': '3633215',
-    'id_customer': '1825',
-    'View%20All': 'Please%20wait...',
-    'type': 'partner',
-    'table': 'customer',
-    'id': '1825',
-    'key': 'id_customer',
-    'cmd': 'mvpnlist_region',
-    'county': 'all',
-    'sort': 'SiteId'
+    "wlkid": "3633215",
+    "id_customer": "1825",
+    "View%20All": "Please%20wait...",
+    "type": "partner",
+    "table": "customer",
+    "id": "1825",
+    "key": "id_customer",
+    "cmd": "mvpnlist_region",
+    "county": "all",
+    "sort": "SiteId"
 }
 
-export_path = './export_telenor_webline_all.csv'
+export_path = "./export_telenor_webline_all.csv"
 
 # Get the date from Webline webserver
 r = requests.post(base_url, data=payload, cookies=cookies, stream=True)
 
 # Create a nice Soup of the result
-soup = BeautifulSoup(r.content, 'html5lib') # Parse the HTML as a string
-table = soup.find_all('table')[3] # Grab a table
+soup = BeautifulSoup(r.content, "html5lib")
+table = soup.find_all("table")[3]
 
 # Put data into a Pandas DataFrame
 df = pd.read_html(str(table))
 df = df[0]
-df['Admin Status'] = df['Admin Status'].map('{:.0f}'.format)
-df['VPN Access Count'] = df['VPN Access Count'].map('{:.0f}'.format)
-df['Site ID'] = df['Site ID'].map('{:.0f}'.format)
+df["Admin Status"] = df["Admin Status"].map("{:.0f}".format)
+df["VPN Access Count"] = df["VPN Access Count"].map("{:.0f}".format)
+df["Site ID"] = df["Site ID"].map("{:.0f}".format)
 df = df.astype(str)
-#df['Bandwidth'] = df['Bandwidth'].str.replace('Kbits/s','')
-#df['Bandwidth'] = df['Bandwidth'].str.replace(' ','')
-df.to_csv(export_path, encoding='utf-8', index=False)
+df.to_csv(export_path, encoding="utf-8", index=False)
--- get-webline-data-underlay-VPN.py
+++ get-webline-data-underlay-VPN.py
@@ -1,39 +1,38 @@
-import requests
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
 # Access cookie from Webline
 authsession = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzcyMTE2MDAsImV4cGlyYXRpb24iOjEwODAwLCJsYXN0X3JlbW90ZV9pcCI6Ijc3LjE4LjUwLjcxIiwib3RwIjoiZmxhc2gtc21zIiwicmVtb3RlX2lwIjoiNzcuMTguNTAuNzEiLCJzaWQiOiI2Nzk2RUM4MC00N0FBLTExRUMtQTE1OS1GODY2QjFERTQwQjAiLCJ1c2VybmFtZSI6InJvZ2Vrcmk1In0.yd3l2W5IuaLuEij_U7VQZf-wm1iv7AwNaPiQKSqrF6o"
 base_url = "https://webline.telenor.no/mod-perl/webline"
-cookies = {'authsession': authsession}
+cookies = {"authsession": authsession}
 
-## Underlay VPN
+
+# Underlay VPN
 payload = {
-    'wlkid': '3633215',
-    'id_customer': '1825',
-    'Search': 'Please%20wait...',
-    'id_vpn': '1115925',
-    'type': 'partner',
-    'cmd': 'html_customer_vpn',
-    'sort': 'SiteId'
+    "wlkid": "3633215",
+    "id_customer": "1825",
+    "Search": "Please%20wait...",
+    "id_vpn": "1115925",
+    "type": "partner",
+    "cmd": "html_customer_vpn",
+    "sort": "SiteId"
     }
 
-export_path = './export_telenor_webline_underlay_vpn.csv'
+export_path = "./export_telenor_webline_underlay_vpn.csv"
 
 # Get the date from Webline webserver
 r = requests.post(base_url, data=payload, cookies=cookies, stream=True)
 
 # Create a nice Soup of the result
-soup = BeautifulSoup(r.content, 'html5lib') # Parse the HTML as a string
-table = soup.find_all('table')[4] # Grab a table
+soup = BeautifulSoup(r.content, "html5lib")
+table = soup.find_all("table")[4]
 
 # Put data into a Pandas DataFrame
 df = pd.read_html(str(table))
 df = df[0]
-df['Admin Status'] = df['Admin Status'].map('{:.0f}'.format)
-df['VPN Access ID'] = df['VPN Access ID'].map('{:.0f}'.format)
-df['Site Id'] = df['Site Id'].map('{:.0f}'.format)
+df["Admin Status"] = df["Admin Status"].map("{:.0f}".format)
+df["VPN Access ID"] = df["VPN Access ID"].map("{:.0f}".format)
+df["Site Id"] = df["Site Id"].map("{:.0f}".format)
 df = df.astype(str)
-#df['Bandwidth'] = df['Bandwidth'].str.replace('Kbits/s','')
-#df['Bandwidth'] = df['Bandwidth'].str.replace(' ','')
-df.to_csv(export_path, encoding='utf-8', index=False)
+df.to_csv(export_path, encoding="utf-8", index=False)
