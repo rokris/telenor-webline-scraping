1d4845fbf5f9e703741b8bbf1ecc89396995bff9
Roger Kristiansen <roger.kristiansen@norgesgruppen.no>
2022-02-28 21:58:44 +0100

    Refactoring

--- get-webline-data-underlay-VPN.py
+++ get-webline-data-underlay-VPN.py
@@ -1,14 +1,29 @@
-def main():
-    import pandas as pd
-    import requests
-    from bs4 import BeautifulSoup
+import jwt
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-    # Access cookie from Webline
-    authsession = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzcyMTE2MDAsImV4cGlyYXRpb24iOjEwODAwLCJsYXN0X3JlbW90ZV9pcCI6IjgxLjE2Ni4yMDIuMjI3Iiwib3RwIjoiZmxhc2gtc21zIiwicmVtb3RlX2lwIjoiNzcuMTguNTAuNzE7ODEuMTY2LjIwMi4yMjciLCJzaWQiOiIwQkE5NEE5Ni00N0RGLTExRUMtQkY5My01RTUxQjFERTQwQjAiLCJ1c2VybmFtZSI6InJvZ2Vrcmk1In0.dtccyAzFfdYubYPCM9X_0eCPlOeDCz6-aiSJbVifZbI"  # noqa: E501
-    base_url = "https://webline.telenor.no/mod-perl/webline"
-    cookies = {"authsession": authsession}
+BASE_URL = "https://webline.telenor.no/mod-perl/webline"
+EXPORT_PATH_CSV = "./export_telenor_webline_underlay_vpn.csv"
+EXPORT_PATH_JSON = "./export_telenor_webline_underlay_vpn.json"
 
-    # Underlay VPN
+def getJwt():
+    while True:
+        jwt_string = input("Access cookie: ")
+        try:
+            jwt.decode(
+                jwt_string, algorithms="HS256", 
+                key="", 
+                options={"verify_exp": True, "verify_signature": False}
+                )
+        except:
+            print("Invalid JWT")
+        else:
+            return jwt_string
+        
+def scrapeWebline():
+    # Get all VPN links
+    cookies = {"authsession": getJwt()}
     payload = {
         "wlkid": "3633215",
         "id_customer": "1825",
@@ -19,15 +34,14 @@ def main():
         "sort": "SiteId",
     }
 
-    export_path = "./export_telenor_webline_underlay_vpn.csv"
+    result = requests.post(BASE_URL, data=payload, cookies=cookies, stream=True)
+    return result
 
-    # Get the date from Webline webserver
-    r = requests.post(base_url, data=payload, cookies=cookies, stream=True)
-
+def createDataset(result):
     # Create a nice Soup of the result
-    soup = BeautifulSoup(r.content, "html5lib")
+    soup = BeautifulSoup(result.content, "html5lib")
     table = soup.find_all("table")[4]
-
+    
     # Put data into a Pandas DataFrame
     df = pd.read_html(str(table))
     df = df[0]
@@ -35,8 +49,20 @@ def main():
     df["VPN Access ID"] = df["VPN Access ID"].map("{:.0f}".format)
     df["Site Id"] = df["Site Id"].map("{:.0f}".format)
     df = df.astype(str)
-    df.to_csv(export_path, encoding="utf-8", index=False)
+    
+    return df
 
+def main():
+
+    # Get the date from Webline webserver
+    result = scrapeWebline()
+    
+    # Create Pandas dataset
+    dataset = createDataset(result)
+
+    # Export data to files
+    dataset.to_csv(EXPORT_PATH_CSV, encoding="utf-8", index=False)
+    dataset.to_json(EXPORT_PATH_JSON, index=False, orient = 'table')
 
 if __name__ == "__main__":
     main()
